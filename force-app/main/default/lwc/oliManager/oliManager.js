e16c7a46fc50e478055e5d1e18a40c11ce0be60a
Ricardo <ricardo@Ricardos-MacBook-Pro.local>
2019-12-16 07:34:03 -0400

    Add OLI modal
    - New Add OLI button in OLIManager.
    - Open and Close modal with OLI Form

--- force-app/main/default/lwc/oliManager/oliManager.js
+++ force-app/main/default/lwc/oliManager/oliManager.js
@@ -1,7 +1,25 @@
 import { LightningElement, api, track} from 'lwc';
+import OLI_OBJECT from '@salesforce/schema/OpportunityLineItem';
+import UNIT_PRICE from '@salesforce/schema/OpportunityLineItem.UnitPrice';
+import SERVICE_DATE from '@salesforce/schema/OpportunityLineItem.ServiceDate';
+import QUANTITY from '@salesforce/schema/OpportunityLineItem.Quantity';
+import PRODUCT from '@salesforce/schema/OpportunityLineItem.Product2Id';
+
 
 export default class OliManager extends LightningElement {
-    
+
     @api recordId;
     @track oppId = this.recordId;
+    @track openmodel = false;
+    sfdcObject = OLI_OBJECT;
+    fields = [PRODUCT, UNIT_PRICE, SERVICE_DATE, QUANTITY]
+
+    openmodal() {
+        this.openmodel = true
+
+    }
+    closeModal() {
+        this.openmodel = false
+    } 
+
 }
